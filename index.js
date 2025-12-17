6a315f7ff6eb2f5cc1182ea6c76d1ca023e4c249
Dung <nhocjoin376@gmai.com>
2025-12-18 03:35:04 +0700

    add root route

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-
 const app = express();
-app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-// test route
 app.get("/", (req, res) => {
-  res.send("🚀 API running on Render + MongoDB");
+  res.send("Backend Render chạy OK 🚀");
 });
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("✅ MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on port ${PORT}`);
-    });
-  })
-  .catch(err => console.error(err));
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
